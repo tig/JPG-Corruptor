335164354cc78c4b0cf2ff5e721107d15598c942
Charlie Kindel <charlie@kindel.com>
2012-02-06 13:20:59 -0700

    first commit

---
+++ JPGCorrupt/Form1.Designer.cs
@@ -0,0 +1,212 @@
+﻿namespace JPGCorrupt
+{
+    partial class JPGCorruptForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(JPGCorruptForm));
+            this.openFile = new System.Windows.Forms.OpenFileDialog();
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.fileToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.openToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.chooseTextMenu = new System.Windows.Forms.ToolStripMenuItem();
+            this.toolStripSeparator2 = new System.Windows.Forms.ToolStripSeparator();
+            this.exitToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.corruptToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.FileCorruptBackgroundWorker = new System.ComponentModel.BackgroundWorker();
+            this.statusStrip = new System.Windows.Forms.StatusStrip();
+            this.toolStripLabelImage = new System.Windows.Forms.ToolStripStatusLabel();
+            this.toolStripLabelText = new System.Windows.Forms.ToolStripStatusLabel();
+            this.toolStripProgressBar1 = new System.Windows.Forms.ToolStripProgressBar();
+            this.toolStripLabelCurrent = new System.Windows.Forms.ToolStripStatusLabel();
+            this.menuStrip1.SuspendLayout();
+            this.statusStrip.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // openFile
+            // 
+            this.openFile.FileName = "openFile";
+            this.openFile.Title = "Open File";
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.fileToolStripMenuItem,
+            this.corruptToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(820, 24);
+            this.menuStrip1.TabIndex = 0;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // fileToolStripMenuItem
+            // 
+            this.fileToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.openToolStripMenuItem,
+            this.chooseTextMenu,
+            this.toolStripSeparator2,
+            this.exitToolStripMenuItem});
+            this.fileToolStripMenuItem.Name = "fileToolStripMenuItem";
+            this.fileToolStripMenuItem.Size = new System.Drawing.Size(37, 20);
+            this.fileToolStripMenuItem.Text = "&File";
+            // 
+            // openToolStripMenuItem
+            // 
+            this.openToolStripMenuItem.Image = ((System.Drawing.Image)(resources.GetObject("openToolStripMenuItem.Image")));
+            this.openToolStripMenuItem.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.openToolStripMenuItem.Name = "openToolStripMenuItem";
+            this.openToolStripMenuItem.ShortcutKeys = ((System.Windows.Forms.Keys)((System.Windows.Forms.Keys.Control | System.Windows.Forms.Keys.O)));
+            this.openToolStripMenuItem.Size = new System.Drawing.Size(193, 22);
+            this.openToolStripMenuItem.Text = "&Choose Image";
+            this.openToolStripMenuItem.Click += new System.EventHandler(this.openToolStripMenuItem_Click);
+            // 
+            // chooseTextMenu
+            // 
+            this.chooseTextMenu.Name = "chooseTextMenu";
+            this.chooseTextMenu.ShortcutKeys = ((System.Windows.Forms.Keys)((System.Windows.Forms.Keys.Control | System.Windows.Forms.Keys.T)));
+            this.chooseTextMenu.Size = new System.Drawing.Size(193, 22);
+            this.chooseTextMenu.Text = "C&hoose Text";
+            this.chooseTextMenu.Click += new System.EventHandler(this.chooseTextMenu_Click);
+            // 
+            // toolStripSeparator2
+            // 
+            this.toolStripSeparator2.Name = "toolStripSeparator2";
+            this.toolStripSeparator2.Size = new System.Drawing.Size(190, 6);
+            // 
+            // exitToolStripMenuItem
+            // 
+            this.exitToolStripMenuItem.Name = "exitToolStripMenuItem";
+            this.exitToolStripMenuItem.Size = new System.Drawing.Size(193, 22);
+            this.exitToolStripMenuItem.Text = "E&xit";
+            this.exitToolStripMenuItem.Click += new System.EventHandler(this.exitToolStripMenuItem_Click);
+            // 
+            // corruptToolStripMenuItem
+            // 
+            this.corruptToolStripMenuItem.Name = "corruptToolStripMenuItem";
+            this.corruptToolStripMenuItem.ShortcutKeys = ((System.Windows.Forms.Keys)((System.Windows.Forms.Keys.Control | System.Windows.Forms.Keys.C)));
+            this.corruptToolStripMenuItem.Size = new System.Drawing.Size(60, 20);
+            this.corruptToolStripMenuItem.Text = "&Corrupt";
+            this.corruptToolStripMenuItem.Click += new System.EventHandler(this.corruptToolStripMenuItem_Click);
+            // 
+            // FileCorruptBackgroundWorker
+            // 
+            this.FileCorruptBackgroundWorker.WorkerReportsProgress = true;
+            this.FileCorruptBackgroundWorker.WorkerSupportsCancellation = true;
+            this.FileCorruptBackgroundWorker.DoWork += new System.ComponentModel.DoWorkEventHandler(this.FileCorruptBackgroundWorker_DoWork);
+            this.FileCorruptBackgroundWorker.ProgressChanged += new System.ComponentModel.ProgressChangedEventHandler(this.FileCorruptBackgroundWorker_ProgressChanged);
+            this.FileCorruptBackgroundWorker.RunWorkerCompleted += new System.ComponentModel.RunWorkerCompletedEventHandler(this.FileCorruptBackgroundWorker_RunWorkerCompleted);
+            // 
+            // statusStrip
+            // 
+            this.statusStrip.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.toolStripLabelImage,
+            this.toolStripLabelText,
+            this.toolStripProgressBar1,
+            this.toolStripLabelCurrent});
+            this.statusStrip.Location = new System.Drawing.Point(0, 463);
+            this.statusStrip.Name = "statusStrip";
+            this.statusStrip.Size = new System.Drawing.Size(820, 24);
+            this.statusStrip.TabIndex = 2;
+            this.statusStrip.Text = "statusStrip";
+            // 
+            // toolStripLabelImage
+            // 
+            this.toolStripLabelImage.BorderSides = ((System.Windows.Forms.ToolStripStatusLabelBorderSides)((((System.Windows.Forms.ToolStripStatusLabelBorderSides.Left | System.Windows.Forms.ToolStripStatusLabelBorderSides.Top) 
+            | System.Windows.Forms.ToolStripStatusLabelBorderSides.Right) 
+            | System.Windows.Forms.ToolStripStatusLabelBorderSides.Bottom)));
+            this.toolStripLabelImage.BorderStyle = System.Windows.Forms.Border3DStyle.SunkenOuter;
+            this.toolStripLabelImage.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Text;
+            this.toolStripLabelImage.Name = "toolStripLabelImage";
+            this.toolStripLabelImage.Size = new System.Drawing.Size(44, 19);
+            this.toolStripLabelImage.Text = "image";
+            // 
+            // toolStripLabelText
+            // 
+            this.toolStripLabelText.BorderSides = ((System.Windows.Forms.ToolStripStatusLabelBorderSides)((((System.Windows.Forms.ToolStripStatusLabelBorderSides.Left | System.Windows.Forms.ToolStripStatusLabelBorderSides.Top) 
+            | System.Windows.Forms.ToolStripStatusLabelBorderSides.Right) 
+            | System.Windows.Forms.ToolStripStatusLabelBorderSides.Bottom)));
+            this.toolStripLabelText.BorderStyle = System.Windows.Forms.Border3DStyle.SunkenOuter;
+            this.toolStripLabelText.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Text;
+            this.toolStripLabelText.Name = "toolStripLabelText";
+            this.toolStripLabelText.Size = new System.Drawing.Size(30, 19);
+            this.toolStripLabelText.Text = "text";
+            // 
+            // toolStripProgressBar1
+            // 
+            this.toolStripProgressBar1.Name = "toolStripProgressBar1";
+            this.toolStripProgressBar1.Size = new System.Drawing.Size(100, 18);
+            this.toolStripProgressBar1.Style = System.Windows.Forms.ProgressBarStyle.Continuous;
+            // 
+            // toolStripLabelCurrent
+            // 
+            this.toolStripLabelCurrent.BorderSides = ((System.Windows.Forms.ToolStripStatusLabelBorderSides)((((System.Windows.Forms.ToolStripStatusLabelBorderSides.Left | System.Windows.Forms.ToolStripStatusLabelBorderSides.Top) 
+            | System.Windows.Forms.ToolStripStatusLabelBorderSides.Right) 
+            | System.Windows.Forms.ToolStripStatusLabelBorderSides.Bottom)));
+            this.toolStripLabelCurrent.BorderStyle = System.Windows.Forms.Border3DStyle.SunkenOuter;
+            this.toolStripLabelCurrent.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Text;
+            this.toolStripLabelCurrent.Name = "toolStripLabelCurrent";
+            this.toolStripLabelCurrent.Size = new System.Drawing.Size(49, 19);
+            this.toolStripLabelCurrent.Text = "current";
+            // 
+            // JPGCorruptForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(820, 487);
+            this.Controls.Add(this.statusStrip);
+            this.Controls.Add(this.menuStrip1);
+            this.Name = "JPGCorruptForm";
+            this.Text = "JPGCorrupt";
+            this.Paint += new System.Windows.Forms.PaintEventHandler(this.JPGCorruptForm_Paint);
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.statusStrip.ResumeLayout(false);
+            this.statusStrip.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.OpenFileDialog openFile;
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem fileToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem openToolStripMenuItem;
+        private System.Windows.Forms.ToolStripSeparator toolStripSeparator2;
+        private System.Windows.Forms.ToolStripMenuItem exitToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem corruptToolStripMenuItem;
+        private System.ComponentModel.BackgroundWorker FileCorruptBackgroundWorker;
+        private System.Windows.Forms.StatusStrip statusStrip;
+        private System.Windows.Forms.ToolStripStatusLabel toolStripLabelImage;
+        private System.Windows.Forms.ToolStripProgressBar toolStripProgressBar1;
+        private System.Windows.Forms.ToolStripMenuItem chooseTextMenu;
+        private System.Windows.Forms.ToolStripStatusLabel toolStripLabelText;
+        private System.Windows.Forms.ToolStripStatusLabel toolStripLabelCurrent;
+    }
+}
+
---
+++ JPGCorrupt/Form1.cs
@@ -0,0 +1,368 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.IO;
+using System.Drawing.Imaging;
+
+namespace JPGCorrupt
+{
+    public partial class JPGCorruptForm : Form
+    {
+        private Random RandomNum = new Random(42);
+        private String SelectedImage;
+        private String CurrentImage;
+        private List<String> WordList;
+        private String OutputDir = "";
+
+        public JPGCorruptForm()
+        {
+            InitializeComponent();
+
+            toolStripLabelText.Text = "C:\\Code\\JPGCorrupt\\Chapter1.txt";
+            WordList = GetWords(toolStripLabelText.Text);
+
+            this.DoubleBuffered = true;
+        }
+
+        private void openToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            Stream myStream = null;
+            OpenFileDialog openFileDialog1 = new OpenFileDialog();
+
+            openFileDialog1.InitialDirectory = ".";
+            openFileDialog1.Filter = "jpg files (*.jpg)|*.jpg|All files (*.*)|*.*";
+            openFileDialog1.FilterIndex = 2;
+            openFileDialog1.RestoreDirectory = false;
+
+            if (openFileDialog1.ShowDialog() == DialogResult.OK)
+            {
+                // Cancel the background worker
+                FileCorruptBackgroundWorker.CancelAsync();
+  
+                try
+                {
+                    if ((myStream = openFileDialog1.OpenFile()) != null)
+                    {
+                        using (myStream)
+                        {
+                            //pictureBox.Image = (Image)new Bitmap(myStream);
+
+                            SelectedImage = openFileDialog1.FileName;
+                            CurrentImage = SelectedImage;
+                            toolStripLabelImage.Text = SelectedImage;
+                            this.Invalidate();
+                            this.Update();
+                        }
+                    }
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show("Error: Could not read file from disk. Original error: " + ex.Message);
+                }
+            }
+        }
+
+
+        private void chooseTextMenu_Click(object sender, EventArgs e)
+        {
+            Stream myStream = null;
+            OpenFileDialog openFileDialog1 = new OpenFileDialog();
+
+            openFileDialog1.InitialDirectory = ".";
+            openFileDialog1.Filter = "txt files (*.txt)|*.txt|All files (*.*)|*.*";
+            openFileDialog1.FilterIndex = 2;
+            openFileDialog1.RestoreDirectory = false;
+
+            if (openFileDialog1.ShowDialog() == DialogResult.OK)
+            {
+                FileCorruptBackgroundWorker.CancelAsync();
+
+                try
+                {
+                    if ((myStream = openFileDialog1.OpenFile()) != null)
+                    {
+                        WordList = GetWords(openFileDialog1.FileName);
+                        toolStripLabelText.Text = openFileDialog1.FileName;
+                    }
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show("Error: Could not read file from disk. Original error: " + ex.Message);
+                }
+            }
+        }
+
+        /// <summary>
+        /// Background worker context
+        /// </summary>
+        class CorruptWorker
+        {
+            public String StartFileName;
+        }
+
+        private void corruptToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            if (corruptToolStripMenuItem.Text == "&Stop")
+            {
+                FileCorruptBackgroundWorker.CancelAsync();
+                toolStripLabelImage.Text = "Cancelled";
+                toolStripProgressBar1.Value = 0;
+            }
+            else
+            {
+                if (!String.IsNullOrEmpty(SelectedImage))
+                {
+                    corruptToolStripMenuItem.Text = "&Stop";
+                    CorruptWorker worker = new CorruptWorker
+                    {
+                        StartFileName = OutputDir + "Corrupt 0000.jpg"
+                    };
+
+                    System.IO.File.Copy(SelectedImage, worker.StartFileName, true);
+                    FileCorruptBackgroundWorker.RunWorkerAsync(worker);
+                }
+            }
+        }
+
+        /// <summary>
+        /// Corrupts a image file by inserting corruptText at a random place
+        /// within the image data.
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void CorruptImageFile(String fileName, String corruptText)
+        {
+            FileInfo file = new FileInfo(fileName);
+            using (FileStream stream = file.Open(FileMode.Open, FileAccess.ReadWrite))
+            {
+                byte[] corruptTextBytes = Encoding.ASCII.GetBytes(corruptText);
+                byte[] bytes = null;
+
+                int len = (int)stream.Length;
+                if (false)
+                {
+                    bytes = new byte[len + corruptText.Length];
+                    stream.Read(bytes, 0, len);
+
+                    // Skip the first 256 bytes 
+                    // TODO: Test whether this is really needed or not. Or if it can be a smaller
+                    // number.
+                    int rnd = RandomNum.Next(256, len);
+
+                    // Shift original data
+                    System.Buffer.BlockCopy(bytes, rnd, bytes, rnd + corruptTextBytes.Length, corruptTextBytes.Length);
+
+                    // Copy in corrupt text
+                    System.Buffer.BlockCopy(corruptTextBytes, 0, bytes, rnd, corruptTextBytes.Length);
+                }
+                else
+                {
+                    bytes = new byte[len];
+                    stream.Read(bytes, 0, len);
+
+                    int rnd = RandomNum.Next(256, len - corruptTextBytes.Length);
+
+                    // Copy in corrupt text
+                    System.Buffer.BlockCopy(corruptTextBytes, 0, bytes, rnd, corruptTextBytes.Length);
+                }
+
+                stream.Seek(0, 0);
+                stream.Write(bytes, 0, bytes.Length);
+                stream.Close();
+            }
+        }
+
+        /// <summary>
+        /// Corrupts a Bitmap in memory by randomly inserting corruptText
+        /// into the RGB data
+        /// </summary>
+        /// <param name="image"></param>
+        /// <param name="corruptText"></param>
+        private void CorruptImage(Bitmap image, String corruptText)
+        {
+            Rectangle rect = new Rectangle(0, 0, image.Width, image.Height);
+            BitmapData bmpData =
+                image.LockBits(rect, System.Drawing.Imaging.ImageLockMode.ReadWrite,
+                image.PixelFormat);
+
+            // Declare an array to hold the bytes of the bitmap.
+            int bytes = Math.Abs(bmpData.Stride) * image.Height;
+            byte[] rgbValues = new byte[bytes];
+
+            // Get the address of the first line.
+            IntPtr ptr = bmpData.Scan0;
+
+            // Copy the RGB values into the array.
+            System.Runtime.InteropServices.Marshal.Copy(ptr, rgbValues, 0, bytes);
+
+            byte[] corruptTextBytes = Encoding.ASCII.GetBytes(corruptText);
+
+            int i = 0;
+            int rnd = RandomNum.Next(bytes - corruptTextBytes.Length);
+            foreach (byte b in corruptTextBytes)
+            {
+                rgbValues[rnd + i] = b;
+            }
+
+            // Copy the RGB values back to the bitmap
+            System.Runtime.InteropServices.Marshal.Copy(rgbValues, 0, ptr, bytes);
+
+            // Unlock the bits.
+            image.UnlockBits(bmpData);
+        }
+
+        private class FileCorruptBackgroundWorkerUserState
+        {
+            public String Image { get; set;}
+            public String Word { get; set; }
+        }
+        
+
+        /// <summary>
+        /// Called by the background worker each time a corrupted image is created
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void FileCorruptBackgroundWorker_ProgressChanged(object sender, ProgressChangedEventArgs e)
+        {
+            // UserState contains the newly corrupted filename
+            //String file = e.UserState as String;
+            //pictureBox.Load(file);
+
+            FileCorruptBackgroundWorkerUserState state = e.UserState as FileCorruptBackgroundWorkerUserState;
+            this.Invalidate();
+            this.Update();
+
+            toolStripLabelCurrent.Text = state.Image + ", " + state.Word;
+
+            toolStripProgressBar1.Value = e.ProgressPercentage;
+                  
+        }
+
+        /// <summary>
+        /// Given a text file, return a list of words.
+        /// </summary>
+        /// <param name="file"></param>
+        private List<String> GetWords(String file)
+        {
+            List<String> list = null;
+            FileInfo txtFile = new FileInfo(file);
+            
+            using (TextReader rdr = txtFile.OpenText())
+            {
+                list = new List<string>();
+                String line;
+                while ((line = rdr.ReadLine()) != null)
+                {
+                    var words = line.Split(' ');
+                    foreach (string word in words)
+                    {
+                        list.Add(word);
+                    }
+                }
+            }
+            return list;
+        }
+
+        /// <summary>
+        /// Worker method for the background worker.
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void FileCorruptBackgroundWorker_DoWork(object sender, DoWorkEventArgs e)
+        {
+            CorruptWorker worker = e.Argument as CorruptWorker;
+
+            ProcessWordsKeepFiles(worker.StartFileName);
+        }
+
+        /// <summary>
+        /// Process words without creating a file for each
+        /// </summary>
+        /// <param name="StartFileName"></param>
+        private void ProcessWords(String StartFileName)
+        {
+
+        }
+
+        /// <summary>
+        /// Process words creating a new file for each word
+        /// </summary>
+        /// <param name="StartFileName"></param>
+        private void ProcessWordsKeepFiles(String StartFileName)
+        {
+            String FileName = StartFileName;
+            String nextFileName;
+            int fileNum = 0;
+
+            FileCorruptBackgroundWorkerUserState UserState = new FileCorruptBackgroundWorkerUserState();
+
+            FileName = String.Format(OutputDir + "Corrupt {0:D4}.jpg", fileNum++);
+            foreach (string word in WordList)
+            {
+                if (FileCorruptBackgroundWorker.CancellationPending)
+                {
+                    return;
+                }
+
+                nextFileName = String.Format(OutputDir + "Corrupt {0:D4}.jpg", fileNum);
+                System.IO.File.Copy(FileName, nextFileName, true);
+                FileName = nextFileName;
+
+                // This reads then writes to the file
+                CorruptImageFile(FileName, word);
+                //System.Threading.Thread.Sleep(500);
+
+                UserState.Image = FileName;
+                UserState.Word = word;
+                FileCorruptBackgroundWorker.ReportProgress((int)((double)fileNum / (double)WordList.Count) * 100, UserState);
+                fileNum++;
+            }
+        }
+
+        private void exitToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            FileCorruptBackgroundWorker.CancelAsync();
+            Application.Exit();
+        }
+
+        private void FileCorruptBackgroundWorker_RunWorkerCompleted(object sender, RunWorkerCompletedEventArgs e)
+        {
+            corruptToolStripMenuItem.Text = "&Corrupt";
+            toolStripLabelCurrent.Text = "Not running";
+        }
+
+        private void JPGCorruptForm_Paint(object sender, PaintEventArgs e)
+        {
+            //// paint the current image
+            //// Create image.
+            System.Diagnostics.Debug.WriteLine(String.Format("Paint {0}", CurrentImage));
+            if (!String.IsNullOrEmpty(CurrentImage))
+            {
+                Image newImage = Image.FromFile(CurrentImage);
+
+                //// Create point for upper-left corner of image.
+                Point ulCorner = new Point(0,20);
+
+                //// Draw image to screen.
+                e.Graphics.DrawImageUnscaled(newImage, ulCorner);
+
+                //// Create font and brush.
+                Font drawFont = new Font("Arial", 16);
+                SolidBrush drawBrush = new SolidBrush(Color.Red);
+
+                //// Create point for upper-left corner of drawing.
+                PointF drawPoint = new PointF(10F, 30F);
+
+                e.Graphics.DrawString(toolStripLabelCurrent.Text, drawFont, drawBrush, drawPoint);
+            }
+        }
+
+    }
+
+}
