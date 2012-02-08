01e41c4e7da524b8df4e7c9765917210d153387d
Charlie Kindel <charlie@kindel.com>
2012-02-08 14:58:57 -0800

    really long test

--- JPGCorrupt/Properties/AssemblyInfo.cs
+++ JPGCorrupt/Properties/AssemblyInfo.cs
@@ -10,7 +10,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("JPGCorrupt")]
-[assembly: AssemblyCopyright("Copyright ©  2012")]
+[assembly: AssemblyCopyright("Copyright © Charlie Kindel 2012")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
